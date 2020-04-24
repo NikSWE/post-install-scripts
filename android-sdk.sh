d0e160e3c996af1e9ef068ef177a8379fe7f09a5
Nikhil Anand <nick.muj.programmer@gmail.com>
2020-04-24 07:16:52 +0000

    Bugfix
    Fixes temporary path modification.

--- android-sdk.sh
+++ android-sdk.sh
@@ -2,7 +2,7 @@ yay -S android-sdk
 sudo chown -R $(whoami):$(whoami) /opt/android-sdk
 mkdir $HOME/.android
 touch $HOME/.android/repositories.cfg
-export PATH=$PATH:/opt/android-sdk/tools/bin
+export PATH="$PATH:/opt/android-sdk/tools/bin"
 sdkmanager "platforms;android-29"
 sdkmanager "platform-tools"
 sdkmanager "build-tools;29.0.3"
