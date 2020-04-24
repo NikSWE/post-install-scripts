d53e80d7e88f929942d9ffb3a5002d646506badb
muj-programmer <nick.muj.programmer@gmail.com>
2020-04-24 12:22:10 +0530

    Update script
    Complete and Test android-sdk install script.

---
+++ android-sdk.sh
@@ -0,0 +1,11 @@
+sudo pacman -S android-sdk
+chown -R $(whoami):$(whoami) /opt/android-sdk
+mkdir $HOME/.android
+touch $HOME/.android/repositories.cfg
+export PATH=$PATH:/opt/android-sdk/tools/bin
+sdkmanager "platforms;android-29"
+sdkmanager "platform-tools"
+sdkmanager "build-tools;29.0.3"
+sdkmanager --licenses
+
+echo "Installed at /opt/android-sdk"
